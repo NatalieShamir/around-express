9c10c1053fa48d05ce0d55dd9fe0af7ad6a52a6e
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-02-26 15:39:48 +0200

    install mongoose and connect to mongodb server

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const { PORT = 3000 } = process.env;
 const { userRouter } = require('./routes/users');
 const { cardRouter } = require('./routes/cards');
+
+mongoose.connect('mongodb://localhost:27017/aroundb');
 
 app.use('/users', userRouter);
 app.use('/cards', cardRouter);
