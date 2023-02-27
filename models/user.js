39b7b950d20b54af8103a4337ccfe6494e0b2f65
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-02-27 09:45:39 +0200

    create user schema and add user schema fields

--- models/user.js
+++ models/user.js
@@ -1,1 +1,20 @@
 const mongoose = require('mongoose');
+
+const userScheme = mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true
+  },
+  about: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true
+  },
+  avatar: {
+    type: String,
+    required: true
+  }
+})
