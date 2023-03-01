94e23e207103cc79751592a39d2fd6abf3d0d041
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-03-01 12:55:10 +0200

    install and implement body-parser

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-const app = express();
 const { PORT = 3000 } = process.env;
-const { userRouter } = require('./routes/users');
-const { cardRouter } = require('./routes/cards');
+const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/aroundb');
 
