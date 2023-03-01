0666c3ba0bdc4cd36145d0fdaa3cd8d78e040edf
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-03-01 10:53:30 +0200

    add missing / to path inside post method

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ const { getAllUsers, getUser, createUser } = require('../controllers/users');
 
 router.get('/users', getAllUsers);
 router.get('/users/:userId', getUser);
-router.post('users', createUser);
+router.post('/users', createUser);
 
 module.exports = {
   userRouter: router,
