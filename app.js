aa7f54702399dad16b92c3f1f70259d924bd53f6
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-04-10 11:27:38 +0300

    set header that allows browser to make requests from any source

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(function (req, res, next) {
   const { origin } = req.headers;
 
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', "*");
   }
 
   req.user = {
