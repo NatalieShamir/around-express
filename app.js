c3d0fc8104abd2a1321e861f5115fc7f2860e651
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-04-10 12:22:39 +0300

    finish processing request and return result to client

--- app.js
+++ app.js
@@ -39,6 +39,8 @@ app.use((req, res, next) => {
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
+
+    return res.end();
   }
 
   req.user = {
