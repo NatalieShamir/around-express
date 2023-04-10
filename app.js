7736bd800fc1f0927a1f660a619566f37cf578de
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-04-10 11:51:55 +0300

    save headers list of original request

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use((req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+  const requestHeaders = req.headers['access-control-request-headers'];
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', '*');
